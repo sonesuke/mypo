28d9d3f356cb88803c5f864dc9402d1af3a6ad2f
sonesuke <iamsonesuke@gmail.com>
2021-02-21 22:17:40 +0900

    fix build.sh

--- build.sh
+++ build.sh
@@ -1,1 +1,8 @@
-poetry update && poetry run pytest tests && poetry config http-basic.pypi "__token__" "${PYPI_API_TOKEN}" &&	poetry publish --build
+#!/bin/bash
+
+set -eu
+poetry update
+poetry run pytest tests
+poetry config http-basic.pypi "__token__" "${PYPI_API_TOKEN}"
+poetry build
+poetry publish
