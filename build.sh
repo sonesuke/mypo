4a6e53c8c8b68e7db9bfb583b16f56f332b731d2
sonesuke <iamsonesuke@gmail.com>
2021-02-21 21:02:10 +0900

    mypo-9 publish pypi

--- build.sh
+++ build.sh
@@ -1,1 +1,1 @@
-poetry update && poetry run pytest tests && oetry config http-basic.pypi "__token__" "${PYPI_API_TOKEN}" &&	poetry publish --build
+poetry update && poetry run pytest tests && poetry config http-basic.pypi "__token__" "${PYPI_API_TOKEN}" &&	poetry publish --build
