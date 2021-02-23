14d3ab704072ce5a07eacde23235889ff40b3345
sonesuke <iamsonesuke@gmail.com>
2021-02-23 16:04:57 +0900

    mypo-13 add type hint

--- mypo/__init__.py
+++ mypo/__init__.py
@@ -1,11 +1,8 @@
 # flake8: noqa
-__version__ = '0.0.1'
+__version__ = "0.0.1"
 
-from .common import calc_capital_gain_tax
-from .common import calc_fee
-from .common import calc_income_gain_tax
-from .runner import Runner
+from .common import calc_capital_gain_tax, calc_fee, calc_income_gain_tax
 from .loader import Loader
 from .market import Market
-from .rebalancer import PlainRebalancer
-from .rebalancer import MonthlyRebalancer
+from .rebalancer import MonthlyRebalancer, PlainRebalancer
+from .runner import Runner
--- tests/test_rebalancer.py
+++ tests/test_rebalancer.py
@@ -1,65 +1,40 @@
+from datetime import date
+
 import numpy as np
 import numpy.testing as npt
-from mypo import PlainRebalancer, MonthlyRebalancer
-from datetime import date
+
+from mypo import MonthlyRebalancer, PlainRebalancer
 
 
 def test_rebalance():
     assets = [1, 1]
     weights = [0.6, 0.4]
-    rebalancer = PlainRebalancer(
-        weights=weights
-    )
+    rebalancer = PlainRebalancer(weights=weights)
     diff = rebalancer.apply(date.today(), assets, 0.5)
-    npt.assert_almost_equal(
-        (assets + diff) / np.sum(assets + diff),
-        weights
-    )
-    npt.assert_almost_equal(
-        np.sum(diff),
-        0
-    )
+    npt.assert_almost_equal((assets + diff) / np.sum(assets + diff), weights)
+    npt.assert_almost_equal(np.sum(diff), 0)
 
 
 def test_rebalance_imbalance():
     assets = np.array([1.3, 1])
     weights = np.array([0.3, 0.7])
-    rebalancer = PlainRebalancer(
-        weights=weights
-    )
+    rebalancer = PlainRebalancer(weights=weights)
     diff = rebalancer.apply(date.today(), assets, 0.5)
-    npt.assert_almost_equal(
-        (assets + diff) / np.sum(assets + diff),
-        weights
-    )
-    npt.assert_almost_equal(
-        np.sum(diff),
-        0
-    )
+    npt.assert_almost_equal((assets + diff) / np.sum(assets + diff), weights)
+    npt.assert_almost_equal(np.sum(diff), 0)
 
 
 def test_rebalance_monthly_fire():
     assets = np.array([1.5, 1])
     weights = np.array([0.3, 0.7])
-    rebalancer = MonthlyRebalancer(
-        weights=weights
-    )
+    rebalancer = MonthlyRebalancer(weights=weights)
     diff = rebalancer.apply(date(2021, 2, 15), assets, 0.5)
-    npt.assert_almost_equal(
-        (assets + diff) / np.sum(assets + diff),
-        weights
-    )
+    npt.assert_almost_equal((assets + diff) / np.sum(assets + diff), weights)
 
 
 def test_rebalance_monthly_not_fire():
     assets = [1.5, 1]
     weights = [0.3, 0.7]
-    rebalancer = MonthlyRebalancer(
-        weights=weights,
-        old_month=2
-    )
+    rebalancer = MonthlyRebalancer(weights=weights, old_month=2)
     diff = rebalancer.apply(date(2021, 2, 15), assets, 0.5)
-    npt.assert_almost_equal(
-        np.sum(np.abs(diff)),
-        0
-    )
+    npt.assert_almost_equal(np.sum(np.abs(diff)), 0)
