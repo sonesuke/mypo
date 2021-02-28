9036e6eb905c0fb36b1dc78dc333abdd569513f1
sonesuke <iamsonesuke@gmail.com>
2021-02-28 11:00:25 +0900

    mypo-10 add threshold trigger

--- tests/test_rebalancer.py
+++ tests/test_rebalancer.py
@@ -3,7 +3,7 @@ from datetime import date
 import numpy as np
 import numpy.testing as npt
 
-from mypo import MonthlyRebalancer, PlainRebalancer
+from mypo import MonthlyRebalancer, PlainRebalancer, ThresholdRebalancer
 
 
 def test_rebalance():
@@ -36,5 +36,21 @@ def test_rebalance_monthly_not_fire():
     assets = [1.5, 1]
     weights = [0.3, 0.7]
     rebalancer = MonthlyRebalancer(weights=weights, old_month=2)
+    diff = rebalancer.apply(date(2021, 2, 15), assets, 0.5)
+    npt.assert_almost_equal(np.sum(np.abs(diff)), 0)
+
+
+def test_rebalance_threshold_fire():
+    assets = np.array([1.5, 1])
+    weights = np.array([0.3, 0.7])
+    rebalancer = ThresholdRebalancer(weights=weights)
+    diff = rebalancer.apply(date(2021, 2, 15), assets, 0.5)
+    npt.assert_almost_equal((assets + diff) / np.sum(assets + diff), weights)
+
+
+def test_rebalance_threshold_not_fire():
+    assets = [1.5, 1]
+    weights = [0.3, 0.7]
+    rebalancer = ThresholdRebalancer(weights=weights, threashold=0.5)
     diff = rebalancer.apply(date(2021, 2, 15), assets, 0.5)
     npt.assert_almost_equal(np.sum(np.abs(diff)), 0)
