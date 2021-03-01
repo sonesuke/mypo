85e461882a4307b6ac8b78a2268930f43bcf21c2
sonesuke <iamsonesuke@gmail.com>
2021-03-01 22:32:07 +0900

    mypo-28 refactoring loss functions

--- mypo/loss_function/loss_function.py
+++ mypo/loss_function/loss_function.py
@@ -33,16 +33,8 @@ def max_drawdown(report: pd.DataFrame) -> np.float64:
     -------
         Negative tatal return.
     """
-    total_assets = list(report["total_assets"])
-    max_assets = total_assets[0]
-    minimum_drawdown = np.float64(1e6)
-    for a in total_assets:
-        if a >= max_assets:
-            max_assets = a
-        this_drawdown = a / max_assets
-        if this_drawdown < minimum_drawdown:
-            minimum_drawdown = this_drawdown
-    return np.float64(np.min(minimum_drawdown))
+    max_assets = report["total_assets"].cummax()
+    return np.float64(np.min(report["total_assets"] / max_assets))
 
 
 def max_drawdown_span(report: pd.DataFrame) -> int:
@@ -58,18 +50,9 @@ def max_drawdown_span(report: pd.DataFrame) -> int:
     -------
         Negative tatal return.
     """
-    total_assets = list(report["total_assets"])
-    max_assets = total_assets[0]
-    not_update_max_of_asset = 0
-    drawdown_span = 0
-    for a in total_assets:
-        if a >= max_assets:
-            not_update_max_of_asset = 0
-            max_assets = a
-        else:
-            not_update_max_of_asset += 1
-
-        if not_update_max_of_asset > drawdown_span:
-            drawdown_span = not_update_max_of_asset
-
-    return drawdown_span
+    df = report[["total_assets"]].copy()
+    df["draw_down"] = df["total_assets"] < df["total_assets"].cummax()
+    ret: int = np.max(
+        df.groupby((df["draw_down"] != df["draw_down"].shift()).cumsum()).cumcount() + 1
+    )
+    return ret
