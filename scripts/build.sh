0e27c493d19b62964c2b6242cef349a6ea40dc53
sonesuke <iamsonesuke@gmail.com>
2021-03-08 22:47:38 +0900

    fix build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,9 +2,9 @@
 
 set -eu
 poetry install
-run flake8 .
-run mypy .
-run pytest tests
+flake8 .
+mypy .
+pytest tests
 poetry config http-basic.pypi "__token__" "${PYPI_API_TOKEN}"
 poetry build
 poetry publish
