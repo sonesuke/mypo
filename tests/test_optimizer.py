b0e08a540d700afb21ffeb2967a39ed0f0ff5db7
sonesuke <iamsonesuke@gmail.com>
2021-03-09 22:36:01 +0900

    mypo-32 sharp ratio optimizer

--- tests/test_optimizer.py
+++ tests/test_optimizer.py
@@ -4,7 +4,7 @@ import os
 import numpy.testing as npt
 
 from mypo import Market
-from mypo.optimizer import MinimumVarianceOptimizer
+from mypo.optimizer import MinimumVarianceOptimizer, SharpRatioOptimizer
 
 TEST_DATA = os.path.join(os.path.dirname(__file__), "data", "test.bin")
 
@@ -23,3 +23,11 @@ def test_minimum_variance_optimizer_with_minimum_return():
     optimizer = MinimumVarianceOptimizer(market=market)
     weights = optimizer.optimize_weight(minimum_return=0.01)
     npt.assert_almost_equal(weights, [0, 1])
+
+
+def test_minimum_sharp_ratio_optimizer():
+    market = Market.load(TEST_DATA)
+    market = market.extract(market.get_index()[:300])
+    optimizer = SharpRatioOptimizer(market)
+    weights = optimizer.optimize_weight()
+    npt.assert_almost_equal(weights, [0.2493479, 0.7506521])
