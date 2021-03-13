3375ac42c6605f6b0db85995f65202d18c2fcafa
sonesuke <iamsonesuke@gmail.com>
2021-03-14 08:21:11 +0900

    mypo-69 refactoring requirements.txt

--- mypo/__init__.py
+++ mypo/__init__.py
@@ -1,5 +1,5 @@
 # flake8: noqa
-__version__ = "0.0.42"
+__version__ = "0.0.0" # refresh automatically by CI
 
 from .common import calc_capital_gain_tax, calc_fee, calc_income_gain_tax
 from .loader import Loader
--- scripts/build.sh
+++ scripts/build.sh
@@ -1,14 +1,31 @@
 #!/bin/bash
 
 set -eu
+
+# change version
+if [ $# != 1 ]
+then
+  echo "bash build.sh <version-number>"
+  exit 1
+fi
+sed -e "s/__version__ = \".*\"/__version__ = \"${1}\"/" -i /app/mypo/__init__.py
+poetry version ${1}
+sed -e "s/assert __version__ == \".*\"/assert __version__ == \"${1}\"/" -i /app/tests/test_mypo.py
+
+# install libraries
 poetry install
+
+# test
 flake8 .
 mypy .
 pytest tests
+
+# build and publish
 poetry config http-basic.pypi "__token__" "${PYPI_API_TOKEN}"
 poetry build
 poetry publish
 
+# build documentation
 sphinx-apidoc -f -o ./docs .
 cd docs
 make html
