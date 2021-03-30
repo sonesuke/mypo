1377bc47f55186bf55208e2ba1eafbed9ee3396f
sonesuke <iamsonesuke@gmail.com>
2021-03-30 23:52:40 +0900

    mypo-126 downgrade python to 3.7.10

--- mypo/settings.py
+++ mypo/settings.py
@@ -1,6 +1,5 @@
 """Parameters for runner."""
 from dataclasses import dataclass
-from typing import Final
 
 import numpy as np
 
@@ -13,4 +12,4 @@ class Settings:
     fee_rate: np.float64
 
 
-DEFAULT_SETTINGS: Final[Settings] = Settings(tax_rate=np.float64(0.20), fee_rate=np.float64(0.005))
+DEFAULT_SETTINGS: Settings = Settings(tax_rate=np.float64(0.20), fee_rate=np.float64(0.005))
