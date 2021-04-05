1a5b09b21c3ad9f0086ca442bb84f3480e46351b
sonesuke <iamsonesuke@gmail.com>
2021-04-05 22:46:12 +0900

    all tickers

--- mypo/model_selection.py
+++ mypo/model_selection.py
@@ -1,6 +1,6 @@
 """Utility functions for model selection."""
 import itertools
-from typing import List, Tuple
+from typing import Any, List, Tuple
 
 import numpy as np
 import pandas as pd
@@ -66,3 +66,50 @@ def make_combinations(cluster: pd.DataFrame) -> List[Tuple[str]]:
     for i in range(k):
         clusters += [list(cluster[cluster["class"] == i].index)]
     return list(itertools.product(*clusters))
+
+
+def evaluate_combinations(market: Market, combinations: List[Tuple[str]], verbose: bool = False) -> pd.DataFrame:
+    """Evaluate combinations.
+
+    Args:
+        combinations: Combinations.
+
+    Returns:
+        Result.
+    """
+    import multiprocessing
+    from datetime import datetime
+
+    from tqdm import tqdm
+
+    from mypo.optimizer import MinimumVarianceOptimizer
+
+    def proc(c: Any) -> Any:
+        optimizer = MinimumVarianceOptimizer(span=50000)
+        target_market = market.filter(list(c))
+        optimizer.optimize(target_market, at=datetime.today())
+        w = optimizer.get_weights()
+        r = target_market.get_rate_of_change().to_numpy()
+        q = np.dot(np.dot(w, np.cov(r.T)), w.T)
+        r = np.dot(w, r.mean(axis=0))
+        return target_market.get_tickers(), r, q, w
+
+    def wrap(x: Any, total: Any) -> Any:
+        """Wrapper for tqdm."""
+        return tqdm(x, total) if verbose else x
+
+    cores = multiprocessing.cpu_count()
+    with multiprocessing.Pool(cores) as pool:
+        imap = pool.imap(proc, combinations)
+        result = list(wrap(imap, total=len(combinations)))
+
+    df = pd.DataFrame(
+        {
+            "c": [r[0] for r in result],
+            "r": [r[1] for r in result],
+            "q": [r[2] for r in result],
+            "w": [r[3] for r in result],
+        }
+    )
+    df["sharp ratio"] = df["r"] / df["q"]
+    return df
