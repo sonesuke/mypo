b492dc3b1d16feb78dabcf4d5b8f94e66202bc82
sonesuke <iamsonesuke@gmail.com>
2021-04-08 21:54:49 +0900

    mypo-142 add maximum Diversification portfolio

---
+++ mypo/optimizer/maximum_diversification_optimizer.py
@@ -0,0 +1,66 @@
+"""Optimizer for weights of portfolio."""
+
+from datetime import datetime
+
+import numpy as np
+from scipy.optimize import minimize
+
+from mypo.common import safe_cast
+from mypo.market import Market
+from mypo.optimizer import BaseOptimizer
+
+
+class MaximumDiversificationOptimizer(BaseOptimizer):
+    """Minimum variance optimizer."""
+
+    _span: int
+    _risk_free_rate: float
+
+    def __init__(
+        self,
+        span: int = 260,
+    ):
+        """Construct this object.
+
+        Args:
+            span: Span for evaluation.
+        """
+        self._span = span
+        super().__init__([1])
+
+    def optimize(self, market: Market, at: datetime) -> None:
+        """Optimize weights.
+
+        Args:
+            market: Past market stock prices.
+            at: Current date.
+
+        Returns:
+            Optimized weights
+        """
+        historical_data = market.extract(market.get_index() < at).get_rate_of_change()
+        prices = historical_data.tail(n=self._span).to_numpy()
+        Q = np.cov(prices.T)
+        Q_diag = np.diag(Q)
+        n = Q.shape[0]
+        x = np.ones(n) / n
+
+        def fn(
+            x: np.ndarray,
+            Q: np.ndarray,
+            Q_diag: np.ndarray,
+        ) -> np.float64:
+            ret: np.float64 = -np.dot(x, Q_diag) / np.dot(np.dot(x, Q), x.T)
+            return ret
+
+        cons = {"type": "eq", "fun": lambda x: np.sum(x) - 1}
+        bounds = [[0.0, 1.0] for i in range(n)]
+        minout = minimize(
+            fn,
+            x,
+            args=(Q, Q_diag),
+            method="SLSQP",
+            bounds=bounds,
+            constraints=cons,
+        )
+        self._weights = safe_cast(minout.x)
