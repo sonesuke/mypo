81053c091fe729b0638fb4f66465f935c792fdc6
sonesuke <iamsonesuke@gmail.com>
2021-04-18 10:54:09 +0900

    Mypo 158 (#159)
    * update image
    
    * add regression selector

--- mypo/__init__.py
+++ mypo/__init__.py
@@ -4,7 +4,14 @@ __version__ = "0.0.0"  # refresh automatically by CI
 from .common import calc_capital_gain_tax, calc_fee, calc_income_gain_tax
 from .loader import Loader
 from .market import Market, SamplingMethod
-from .model_selection import Fold, clustering_tickers, evaluate_combinations, split_k_folds
+from .model_selection import (
+    Fold,
+    clustering_tickers,
+    evaluate_combinations,
+    select_by_correlation,
+    select_by_regression,
+    split_k_folds,
+)
 from .reporter import Reporter
 from .runner import Runner
 from .settings import Settings
