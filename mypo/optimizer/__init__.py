aaab8a7ce0ffaabd90f96f217094dee3c9560308
sonesuke <iamsonesuke@gmail.com>
2021-06-20 16:35:17 +0900

    mypo-170 mean variance portfolio (#171)

--- mypo/optimizer/__init__.py
+++ mypo/optimizer/__init__.py
@@ -3,6 +3,7 @@
 from mypo.optimizer.base_optimizer import BaseOptimizer
 from mypo.optimizer.cvar_optimizer import CVaROptimizer
 from mypo.optimizer.maximum_diversification_optimizer import MaximumDiversificationOptimizer
+from mypo.optimizer.mean_variance_optimizer import MeanVarianceOptimizer
 from mypo.optimizer.minimum_variance_optimizer import MinimumVarianceOptimizer
 from mypo.optimizer.no_optimizer import NoOptimizer
 from mypo.optimizer.objective import covariance, semi_covariance
